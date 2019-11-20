a2657a3d1b4b6ce1cb504190f3c231e1da0165ce
Student <example@example.com>
2019-11-20 15:23:14 -0800

    seperated business and UI logic. it all works

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Beer } from './../src/beerfriends.js';
-// import {ApiCall} from './../src/beerfriendsapi.js';
+import {ApiCall} from './../src/beerfriendsapi.js';
 
 $(document).ready(function() {
   $('#beersearch').click(function() {
@@ -17,13 +17,10 @@ $(document).ready(function() {
     };
 
     (async () => {
-  // The API call is business logic.
-  let response = await fetch(`https://api.punkapi.com/v2/beers?${beer.searchQuery}`);
-  let jsonifiedResponse = await response.json();
-  // The getElements method is UI logic so it will need to be separated from the business logic.
-
-  getElements(jsonifiedResponse[0]);
-})();
+      let apiCall = new ApiCall();
+      let jsonifiedResponse = await apiCall.getApiData(beer.searchQuery);
+      getElements(jsonifiedResponse[0]);
+    })();
 
   });
 });
